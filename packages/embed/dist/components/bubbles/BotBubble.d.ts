7d177c638ab9ff1d756bafa0f606fded521df4ad
Max Techera <maxi.techerag@gmail.com>
2024-04-29 15:19:43 -0300

    Add embed dist

---
+++ packages/embed/dist/components/bubbles/BotBubble.d.ts
@@ -0,0 +1,16 @@
+import { MessageType } from '../Bot';
+type Props = {
+    message: MessageType;
+    chatflowid: string;
+    chatId: string;
+    apiHost?: string;
+    fileAnnotations?: any;
+    showAvatar?: boolean;
+    avatarSrc?: string;
+    backgroundColor?: string;
+    textColor?: string;
+    chatFeedbackStatus?: boolean;
+};
+export declare const BotBubble: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=BotBubble.d.ts.map
