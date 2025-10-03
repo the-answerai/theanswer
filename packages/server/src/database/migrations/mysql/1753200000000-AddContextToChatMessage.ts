53b78efe904729ea67cb83924ad083c425971dfa
Jaime Morales <jaime@lastrev.com>
2025-10-03 08:14:48 -0700

    awaiting in mariadb and mysql migration scripts

--- packages/server/src/database/migrations/mysql/1753200000000-AddContextToChatMessage.ts
+++ packages/server/src/database/migrations/mysql/1753200000000-AddContextToChatMessage.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class AddContextToChatMessage1753200000000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         const columnExists = await queryRunner.hasColumn('chat_message', 'context')
-        if (!columnExists) queryRunner.query(`ALTER TABLE \`chat_message\` ADD COLUMN \`context\` LONGTEXT;`)
+        if (!columnExists) await queryRunner.query(`ALTER TABLE \`chat_message\` ADD COLUMN \`context\` LONGTEXT;`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
