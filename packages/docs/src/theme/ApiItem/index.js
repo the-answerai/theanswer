c667e4786990dfedcb5f52e388f76c6db9da25ad
Max Techera <maxi.techerag@gmail.com>
2025-10-03 11:09:23 -0300

    cleanup js built files

--- packages/docs/src/theme/ApiItem/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ApiItemWrapper;
-var ApiItem_1 = require("@theme-original/ApiItem");
-function ApiItemWrapper(props) {
-    return <ApiItem_1.default {...props}/>;
-}
