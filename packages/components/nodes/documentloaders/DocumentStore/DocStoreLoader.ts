50f01a16ba8ce9552d384020770daa64d45280fd
Max Techera <maxi.techerag@gmail.com>
2026-01-20 16:48:42 -0300

    fix(components): remove userId override in dropdowns to respect workspace filtering
    Closes #853

--- packages/components/nodes/documentloaders/DocumentStore/DocStoreLoader.ts
+++ packages/components/nodes/documentloaders/DocumentStore/DocStoreLoader.ts
@@ -65,10 +65,6 @@ class DocStore_DocumentLoaders implements INode {
             }
 
             const searchOptions = options.searchOptions || {}
-            searchOptions.where = {
-                ...searchOptions.where,
-                userId: options.userId
-            }
             const stores = await appDataSource.getRepository(databaseEntities['DocumentStore']).findBy(searchOptions)
             for (const store of stores) {
                 if (store.status === 'SYNC') {
