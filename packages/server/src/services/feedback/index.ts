d1c1dcf4f1c90e59e033e22a6589ad0a0a6f2e17
Diego Costa <diecoscai@gmail.com>
2026-03-18 18:10:52 -0300

    fix(feedback): use structured logger with minimal safe fields

--- packages/server/src/services/feedback/index.ts
+++ packages/server/src/services/feedback/index.ts
@@ -5,6 +5,7 @@ import { utilUpdateChatMessageFeedback } from '../../utils/updateChatMessageFeed
 import { IChatMessageFeedback } from '../../Interface'
 import { InternalFlowiseError } from '../../errors/internalFlowiseError'
 import { getErrorMessage } from '../../errors/utils'
+import logger from '../../utils/logger'
 
 // Get all chatmessage feedback from chatflowid
 const getAllChatMessageFeedback = async (
@@ -31,14 +32,11 @@ const createChatMessageFeedbackForChatflow = async (requestBody: Partial<IChatMe
         const dbResponse = await utilAddChatMessageFeedback(requestBody)
         return dbResponse
     } catch (error) {
-        console.log('[FEEDBACK DEBUG] feedbackService.createChatMessageFeedbackForChatflow error:', JSON.stringify({
+        logger.error('[FEEDBACK DEBUG] feedbackService.createChatMessageFeedbackForChatflow error', {
             code: (error as any).code,
             constraint: (error as any).constraint,
-            message: getErrorMessage(error),
-            messageId: (requestBody as any).messageId,
-            chatId: (requestBody as any).chatId,
-            rating: (requestBody as any).rating
-        }, null, 2))
+            messageId: (requestBody as any).messageId
+        })
         throw new InternalFlowiseError(
             StatusCodes.INTERNAL_SERVER_ERROR,
             `Error: feedbackService.createChatMessageFeedbackForChatflow - ${getErrorMessage(error)}`
--- packages/server/src/utils/addChatMessageFeedback.ts
+++ packages/server/src/utils/addChatMessageFeedback.ts
@@ -1,6 +1,7 @@
 import { ChatMessageFeedback } from '../database/entities/ChatMessageFeedback'
 import { IChatMessageFeedback } from '../Interface'
 import { getRunningExpressApp } from '../utils/getRunningExpressApp'
+import logger from './logger'
 
 /**
  * Method that add chat message feedback.
@@ -15,15 +16,11 @@ export const utilAddChatMessageFeedback = async (chatMessageFeedback: Partial<IC
         const feedback = await appServer.AppDataSource.getRepository(ChatMessageFeedback).create(newChatMessageFeedback)
         return await appServer.AppDataSource.getRepository(ChatMessageFeedback).save(feedback)
     } catch (error) {
-        console.log('[FEEDBACK DEBUG] DB save error:', JSON.stringify({
+        logger.error('[FEEDBACK DEBUG] DB save error', {
             code: (error as any).code,
-            detail: (error as any).detail,
             constraint: (error as any).constraint,
-            driverError: (error as any).driverError?.message,
-            messageId: chatMessageFeedback.messageId,
-            chatId: chatMessageFeedback.chatId,
-            rating: chatMessageFeedback.rating
-        }, null, 2))
+            messageId: chatMessageFeedback.messageId
+        })
         throw error
     }
 }
