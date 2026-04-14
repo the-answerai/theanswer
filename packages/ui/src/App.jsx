6333af138f37c2e47f94a0383cdce458ab97f898
Diego <diecoscai@gmail.com>
2026-04-14 14:03:47 -0300

    chore(ui): remove broken useAuth0 import from legacy App.jsx entry

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import { ThemeProvider } from '@mui/material/styles'
-import { Button, CssBaseline, StyledEngineProvider } from '@mui/material'
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
 // routing
 import Routes from '@/routes'
@@ -13,39 +13,19 @@ import themes from '@/themes'
 
 // project imports
 import NavigationScroll from '@/layout/NavigationScroll'
-import { useAuth0 } from '@auth0/auth0-react'
 import useNotifyParentOfNavigation from './utils/useNotifyParentOfNavigation'
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
     const customization = useSelector((state) => state.customization)
-    const { user, isLoading, getAccessTokenSilently, error, signinWithRedirect } = useAuth0()
+    const user = useSelector((state) => state.auth.user)
     useNotifyParentOfNavigation()
     React.useEffect(() => {
         if (user?.chatflowDomain) {
             sessionStorage.setItem('baseURL', user.chatflowDomain.replace('8080', '4000'))
         }
     }, [user?.chatflowDomain])
-    React.useEffect(() => {
-        ;(async () => {
-            try {
-                // console.log('user', { user, isLoading })
-                if (!user && !isLoading) {
-                    await signinWithRedirect()
-                } else if (user) {
-                    const newToken = await getAccessTokenSilently({
-                        authorizationParams: {
-                            // scope: 'write:admin'
-                        }
-                    })
-                    sessionStorage.setItem('access_token', newToken)
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        })()
-    }, [user, isLoading, getAccessTokenSilently, signinWithRedirect])
 
     return (
         <StyledEngineProvider injectFirst>
@@ -54,12 +34,6 @@ const App = () => {
                 <NavigationScroll>
                     <Routes />
                 </NavigationScroll>
-                {error && (
-                    <>
-                        <h1>{error.message}</h1>
-                        <Button onClick={() => loginWithRedirect()}>Try Again</Button>
-                    </>
-                )}
             </ThemeProvider>
         </StyledEngineProvider>
     )
